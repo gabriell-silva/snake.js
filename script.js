657c2ef1adcbfaa40aaa244bf3e5f8690eeb04d1
gabriell-silva <gabrielsilva.freires2@gmail.com>
2022-05-05 03:22:14 -0300

    finish project snake.js

--- script.js
+++ script.js
@@ -2,51 +2,21 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = "right";
 
-snake[0] = {
-    x: 8 * box,
-    y: 8 * box
-}
-
-let direction = "rigth";
-let food = {
-    x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box,
-}
-
-const createBackground = () => {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box);
-}
-
-const createSnake = () => {
-    for (let index = 0; index < snake.length; index++) {
-        context.fillStyle = "green";
-        context.fillRect(snake[index].x, snake[index].y, box, box);
-        
-    }
-}
-
-const drawFood = () => {
-    context.fillStyle = "red";
-    context.fillRect(food.x, food.y, box, box);
-}
-
-document.addEventListener('keydown', update);
-
-const update = (e) => {
-    if (e.keyCode == 37 && direction != "right") return direction = "left";
-    if (e.keyCode == 38 && direction != "down") return direction = "up";
-    if (e.keyCode == 39 && direction != "left") return direction = "right";
-    if (e.keyCode == 40 && direction != "up") return direction = "down";
-}
-
-const initialGame = () => {
+function initialGame() {
     
-    if (snake[0].x > 15 * box && direction == "right") return snake[0].x = 0;
-    if (snake[0].x < 0 && direction == "left") return snake[0].x = 16 * box;
-    if (snake[0].y > 15 * box && direction == "down") return snake[0].y = 0;
-    if (snake[0].y < 0 && direction == "up" ) return snake[0].y = 16 * box;
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up" ) snake[0].y = 16 * box;
+
+    for (let index = 1; index < snake.length; index++) {
+        if (snake[0].x == snake[index].x && snake[0].y == snake[index].y) {
+            clearInterval(game);
+            alert('Game Over! 😓');          
+        }        
+    }
 
     createBackground();
     createSnake();
@@ -55,10 +25,10 @@ const initialGame = () => {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
     //snake directions
-    if (direction == "right") return snakeX += box;
-    if (direction == "left") return snakeX -= box;
-    if (direction == "up") return snakeY -= box;
-    if (direction == "down") return snakeY += box;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     if (snakeX != food.x || snakeY != food.y) {
         snake.pop();
@@ -79,3 +49,39 @@ const initialGame = () => {
 }
 
 let game = setInterval(initialGame, 100);
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function createBackground() {
+    context.fillStyle = "lightblue";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake() {
+    for (let index = 0; index < snake.length; index++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[index].x, snake[index].y, box, box);
+    }
+}
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+document.addEventListener('keydown', update);
